98c8c4eff9d636d6bad37826de3c604af89a05da
emilymeng <eeng@seattletimes.com>
2019-01-23 13:45:28 -0800

    Revert "sheet json map"
    This reverts commit aba340b3a7c1321f1de5938d6af0ac229d9c3f26.

--- src/js/main.js
+++ src/js/main.js
@@ -15,73 +15,37 @@ ich.addTemplate("popup", templateFile);
 // var onEachFeature = function(feature, layer) {
 //   layer.bindPopup(ich.popup(feature.properties))
 // };
-// var onEachFeature = function(feature, layer) {
-//   layer.bindPopup(ich.popup(feature.properties))
-// };
-
-// var data = require("./sps_lead.geo.json");
-
- var markers = [];
- var latest = null;
- var markergroup = L.featureGroup()
-
- var commafy = s => s.toLocaleString().replace(/\.0+/, "")
- console.log(window.lead)
-
-window.lead.forEach(function(data) {    
-	var marker = L.marker([data.lat, data.lng], {
-     icon: L.divIcon({
-       className: "leaflet-div-icon lead-icon " + data.aboveTen
-   })
-
- });
-
 var onEachFeature = function(feature, layer) {
-  layer.bindPopup(ich.popup(feature.properties))
+  // layer.bindPopup(ich.popup(feature.properties))
+};
 
-  layer.on({
-  	     mouseover: function(e) {
-        layer.setStyle({ weight: 3, fillOpacity: .8 });
-      },
-      mouseout: function(e) {
-        if (focused && focused == layer) { return }
-        layer.setStyle({ weight: 2, fillOpacity: 0.5 });
-      }
-    });
-	};
+var data = require("./sps_lead.geo.json");
 
-// function geojsonMarkerOptions(feature) {
-//   console.log(feature.properties)
+function geojsonMarkerOptions(feature) {
+  console.log(feature.properties)
 
-//   return {
-//     radius: 5,
-//     // fillColor: getColor(feature.properties.type),
-//     fillColor: "#89d359",
-//     color: "#000000",
-//     weight: 1,
-//     opacity: 1,
-//     fillOpacity: 0.6
-//   }
-// };
+  return {
+    radius: 5,
+    // fillColor: getColor(feature.properties.type),
+    fillColor: "#89d359",
+    color: "#000000",
+    weight: 1,
+    opacity: 1,
+    fillOpacity: 0.6
+  }
+};
 
-// var geojson = L.geoJson(data, {
-//     pointToLayer: function (feature, latlng) {
-//         return L.circleMarker(latlng);
-//     },
-//     style: geojsonMarkerOptions,
-//     onEachFeature: onEachFeature
-// }).addTo(map);
+var geojson = L.geoJson(data, {
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng);
+    },
+    style: geojsonMarkerOptions,
+    onEachFeature: onEachFeature
+}).addTo(map);
 
 
 // var onEachFeature = function(feature, layer) {
 //   layer.bindPopup(ich.popup(feature.properties))
 // };
-
-  // marker.bindPopup(html);
-  markers.push(marker);
-  marker.addTo(markergroup);
-});
-
-markergroup.addTo(map);
 
  map.scrollWheelZoom.disable();
