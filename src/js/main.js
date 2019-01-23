b8e532466a02a0465f4c3dab4b5b4159845c4dd1
emilymeng <eeng@seattletimes.com>
2019-01-23 15:30:02 -0800

    side by side maps

--- src/js/main.js
+++ src/js/main.js
@@ -12,26 +12,39 @@ var ich = require("icanhaz");
 var templateFile = require("./_popup.html");
 ich.addTemplate("popup", templateFile);
 
-// var onEachFeature = function(feature, layer) {
-//   layer.bindPopup(ich.popup(feature.properties))
-// };
+var data = require("./SPS-lead.geo.json");
+var mapElement = document.querySelector("leaflet-map.SPS");
+
 var onEachFeature = function(feature, layer) {
-  // layer.bindPopup(ich.popup(feature.properties))
+  layer.bindPopup(ich.popup(feature.properties))
+
+    layer.on({
+      mouseover: function(e) {
+        layer.setStyle({ weight: 3, fillOpacity: .8 });
+      },
+      mouseout: function(e) {
+        if (focused && focused == layer) { return }
+        layer.setStyle({ weight: 1, fillOpacity: 0.6 });
+      }
+    });
 };
 
-var data = require("./sps_lead.geo.json");
+function getColor(d) {
+  return  d >= .21 ? '#d73027' :
+          d >= .11 ? '#f46d43' :
+          d >= .01 ? '#fee090' :
+    '#abd9e9';
+}
 
 function geojsonMarkerOptions(feature) {
-  console.log(feature.properties)
 
   return {
-    radius: 5,
-    // fillColor: getColor(feature.properties.type),
-    fillColor: "#89d359",
+    radius: 6,
+    fillColor: getColor(feature.properties.aboveTen),
     color: "#000000",
     weight: 1,
     opacity: 1,
-    fillOpacity: 0.6
+    fillOpacity: 0.7
   }
 };
 
@@ -43,9 +56,52 @@ var geojson = L.geoJson(data, {
     onEachFeature: onEachFeature
 }).addTo(map);
 
+ map.scrollWheelZoom.disable();
 
-// var onEachFeature = function(feature, layer) {
-//   layer.bindPopup(ich.popup(feature.properties))
-// };
+//second map
+var dataNoah = require("./SPS-lead.geo.json");
+var mapElement2 = document.querySelector("leaflet-map.noah");
+
+var onEachFeature2 = function(feature, layer) {
+  layer.bindPopup(ich.popup(feature.properties))
+
+    layer.on({
+      mouseover: function(e) {
+        layer.setStyle({ weight: 3, fillOpacity: .8 });
+      },
+      mouseout: function(e) {
+        if (focused && focused == layer) { return }
+        layer.setStyle({ weight: 1, fillOpacity: 0.6 });
+      }
+    });
+};
+
+function getColor2(d) {
+  return  d >= .21 ? '#d73027' :
+          d >= .11 ? '#f46d43' :
+          d >= .01 ? '#fee090' :
+    '#abd9e9';
+}
+
+function geojsonMarker2Options(feature) {
+
+  return {
+    radius: 6,
+    fillColor: getColor2(feature.properties.aboveTwo),
+    color: "#000000",
+    weight: 1,
+    opacity: 1,
+    fillOpacity: 0.7
+  }
+};
+
+var geojson = L.geoJson(dataNoah, {
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker2(latlng);
+    },
+    style: geojsonMarker2Options,
+    onEachFeature2: onEachFeature2
+}).addTo(map);
 
  map.scrollWheelZoom.disable();
+
